8186ec3ad39cd3db9271e4ae66521f989207c976
BalaMurugan <balabala13570@gmail.com>
2025-04-30 22:53:41 +0530

    fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,19 +4,44 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+void fibonacci(int num){
+    int f0 = 0, f1 = 1;
+
+    cout << f0 << " ";
+    while(f1 <= num){
+        cout << f1 << " ";
+        int temp = f1 + f0;
+        f0 = f1;
+        f1 = temp;
+    }
+}
+
+void fibonaccin(int num){
+    int f0 = 0, f1 = 1;
+    cout << f0 << " ";
+    for(int i = 2; i <= num; i++){
+        cout << f1 << " ";
+        int temp = f1 + f0;
+        f0 = f1;
+        f1 = temp;
+    }
+}
+
 int main(){
+    system("cls");
     int num = 0;
     cout << "Enter the upper limit for Fibonacci series: ";
     cin >> num;
 
-    int f0 = 0, f1 = 1;
+    if(num <= 1){
+        cout << "The number must be greater than the base number 1." << endl;
+    }
+    else{
+        cout << "Fibonacci series: ";
+        fibonacci(num);
 
-    cout << f0 << endl;
-    while(f1 <= num){
-        cout << f1 << endl;
-        int temp = f1 + f0;
-        f0 = f1;
-        f1 = temp;
+        cout << "\nFibonacci series count: ";
+        fibonaccin(num);
     }
     return 0;
 }
